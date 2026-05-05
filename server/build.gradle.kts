83e35e049560d16043f39b382a59aa9943b130be
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2026-05-05 21:41:32 +0500

    Add gradle shadow jar plugin to include 'shadowJar' task that creates a fat jar (self-contained) file.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,8 +1,23 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     id("java-common")
+    id("com.gradleup.shadow") version "9.4.1"
 }
 
 dependencies {
     implementation(project(":http"))
     implementation(project(":logger"))
+}
+
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("jweb")
+    archiveClassifier.set("") // Removes the "-all" suffix from the jar filename
+    manifest {
+        attributes["Main-Class"] = "nturbo1.WebServerApp"
+    }
+    minimize()
+
+    // Optional: Merges resource files with the same name (e.g., Log4j configs)
+    // mergeServiceFiles()
 }
